3e22cbf8f6272e00ea1aa0f60235791e479b1827
Sudeep Ranjan Sahoo <sudeepranjan@iitbhilai.ac.in>
2023-08-26 16:39:43 +0530

    fix: filename and modified app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import {React,useState,useRef,useEffect} from "react";
+import { React, useState, useRef, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-
+import { Navbar,Footer} from "./components";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
     </div>
   );
 }
