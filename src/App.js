3360c44da69d9f1d9be1eddacadc8f827dd78eb2
Sudeep Ranjan Sahoo <sudeepranjan@iitbhilai.ac.in>
2023-08-26 12:17:59 +0530

    Base setup and folder organisation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import {React,useState,useRef,useEffect} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
